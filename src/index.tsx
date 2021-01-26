b6e1403fa5a0a53f531d991ad11f45e9ca808c8e
xiaomifeng <2603501405@qq.com>
2021-01-26 20:21:44 +0800

    first commit

--- src/index.tsx
+++ src/index.tsx
@@ -8,7 +8,21 @@ ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById('root'),
+  () => {
+    setTimeout(() => {
+      // ?.firstChild 是因为有可能是null,null.firstChild就报错，
+      // 加上个?.firstChild，就可以兼容了
+      // console.log('dsag', document.getElementById('root1')?.firstChild)
+
+      // 卸载的两种操作：1 我们肯定document.getElementById('root')存在的，删掉里面的第一个元素
+      // ((document.getElementById('root') as HTMLElement).firstChild as HTMLElement).remove()
+      
+      //  ReactDOM.unmountComponentAtNode删除
+      // 接收个dom节点作为容器，删掉容器里面的。
+      ReactDOM.unmountComponentAtNode(document.getElementById('root') as HTMLElement)
+    }, 3000)
+  }
 );
 
 // If you want to start measuring performance in your app, pass a function
